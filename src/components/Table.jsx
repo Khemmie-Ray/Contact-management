db38158aac4dc8a400ef6151bca436afcc604133
Khemmie-Ray <atokemmy@gmail.com>
2023-12-12 11:39:07 +0100

    adding edit and delete functionality

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { FaPlusCircle } from "react-icons/fa";
+import { FaPlusCircle, FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 import ModalForm from './ModalForm';
 
 const Table = () => {
   const [showModal, setShowModal] = useState(false);
   const [contacts, setContacts] = useState([]);
+  const [selectedContact, setSelectedContact] = useState(null);
 
   useEffect(() => {
     const storedContacts = JSON.parse(localStorage.getItem('contacts')) || [];
@@ -13,12 +15,45 @@ const Table = () => {
 
   const handleAddClick = () => {
     setShowModal(!showModal);
+    setSelectedContact(null); 
+  };
+
+  const handleEditClick = (contact) => {
+    setShowModal(!showModal);
+    setSelectedContact(contact);
+  };
+
+  const handleDeleteClick = (email) => {
+    const updatedContacts = contacts.filter((contact) => contact.email !== email);
+    setContacts(updatedContacts);
+    localStorage.setItem('contacts', JSON.stringify(updatedContacts));
+  };
+
+
+  const handleFormSubmit = (newContact) => {
+    if (selectedContact) {
+      const updatedContacts = contacts.map((contact) =>
+        contact.email === selectedContact.email ? newContact : contact
+      );
+      setContacts(updatedContacts);
+      localStorage.setItem('contacts', JSON.stringify(updatedContacts));
+    } else {
+      
+      const updatedContacts = [...contacts, newContact];
+      setContacts(updatedContacts);
+      localStorage.setItem('contacts', JSON.stringify(updatedContacts));
+    }
+
+    setShowModal(false);
+    setSelectedContact(null);
   };
 
   return (
     <div className='w-[80%] mx-auto'>
-      <button onClick={handleAddClick} className='bg-gray-400 text-black rounded-md p-4 mb-4 flex items-center justify-between w-[10%]'>Add <FaPlusCircle /></button>
-      <table className='w-[100%] '>
+      <button onClick={handleAddClick} className='bg-gray-400 text-black rounded-md p-4 mb-4 flex items-center justify-between w-[10%]'>
+        Add <FaPlusCircle />
+      </button>
+      <table className='w-[100%]'>
         <thead className='bg-gray-400 p-4'>
           <tr>
             <th className='w-2/6 py-4 text-left pl-2'>Name</th>
@@ -33,15 +68,23 @@ const Table = () => {
               <td className='w-2/6 py-4 text-left pl-2'>{contact.name}</td>
               <td className='w-2/6 py-4 text-left'>{contact.email}</td>
               <td className='w-2/6 py-4 text-left'>{contact.phone}</td>
-              <td className='w-2/6 py-4 text-left'>Actions</td>
+              <td className='w-2/6 py-4 text-left text-[1rem]'>
+                <button onClick={() => handleEditClick(contact)}><FaEdit /></button>
+                <button onClick={() => handleDeleteClick(contact.email)}><MdDelete /></button>
+              </td>
             </tr>
           ))}
         </tbody>
       </table>
 
-      {showModal && <ModalForm  />}
+      {showModal && (
+        <ModalForm
+          contact={selectedContact}
+          onSubmit={handleFormSubmit}
+        />
+      )}
     </div>
   );
-}
+};
 
 export default Table;
